ee0107ce60966ae0c5f658785fe8c0c411e38a85
kenkairon <ingeinfo2006@gmail.com>
2022-12-16 11:10:23 -0300

    variables basicas en javascript

---
+++ Javascript/js/variables.js
@@ -0,0 +1,56 @@
+// Variable es un espacio que guardamos en memoria
+//tipos de variables
+// Existen tipos de Variables(number)
+let numero = 29;
+alert(numero);
+
+//variable string
+var string = "Bienvenido";
+alert (string);
+
+// Variable boleana puede ser positivo o negativo
+let verdadero = true
+alert(verdadero);
+
+// constante esto no cambia esto es fijo  las constante no pueden cambiar su valor
+const pi = 3.14;
+alert(pi);
+
+// undifined: En este caso no hay una variable definida no hay numero,string, boleano?
+let variables1;
+alert(variables1);
+
+let numerativo;
+alert(numerativo);
+numerativo = 3;
+
+// null es valor vacio que lo declaramos intencionalmente
+let num = null;
+alert(num);
+
+// Nam pasa a cuando hacemos operaciones de LETRAS con NUMERO
+// por eso nos da el resultado de NAM
+let num1 = 23;
+let num2 = "carlos";
+alert (num1 * num2);
+
+// multiples variables
+let  nur,nur2,nur3;
+nur1 = 1;
+nur2 = 9;
+nur3 = 13;
+
+alert(nur1);
+alert(nur2);
+alert(nur3);
+
+//prompt es una API de javascript
+// Aqui se guara en la variable nombres
+let nombres = prompt("decime tu nombre");
+alert(nombres);
+
+//Aqui concatenamos con la variable nombres1
+let nombres1 = prompt("dame tu segundo nombre");
+alert("Hola Bienvenido " + nombres1);
+
+
