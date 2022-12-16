56c23c5a5442cdf131ac865197adde44b9893b3e
kenkairon <ingeinfo2006@gmail.com>
2022-12-16 14:33:41 -0300

    Concatenaciones de javascript

---
+++ Javascript/js/concatenacion.js
@@ -0,0 +1,55 @@
+// concatenacion de string
+//saludo = "¡Hola Carlos!";
+//pregunta = " ¿como estas?";
+
+//frase = saludo + pregunta;
+
+//document.write(frase);
+
+// PARA TRABAJAR COMO STRING DOS NUMEROS y JUNTA TODO;
+//numero1 = 53;
+//numero2 = 8;
+
+//frase = "" + numero1 + numero2;
+//document.write(frase);
+
+
+//concatenacion de string con números
+// concat es solo para los string
+//numero3 = "Bienvenido número ";
+//numero4 = 7;
+
+//frase = numero3.concat(numero4);
+//document.write(frase);
+
+//Concatenación  de variables
+//nombre = " Carlos Vasquez";
+//frase  = "Soy " + nombre + " y estoy programando";
+//document.write(frase);
+
+//Concatenación de variables con  ${}  `` = backticks
+
+//nombre = "Carlos vasquez";
+//frase = `Soy ${nombre} y estoy programando`;
+//document.write(frase);
+
+//Concatenación de variables con  ${}  `` = backticks con html
+//nombre2 = "Ximena Saavedra";
+// frasehtml = `<h1>${nombre2}</h1>
+//             <p>programando</p>
+//             <ul>
+//                 <li>Javascript</li>
+//                 <li>Html5</li>
+//                 <li>Css</li>
+//             </ul>`
+//document.write(frasehtml);
+
+// concatenacion con  '""' las combinacion de comillas
+// nombre = "carlos vasquez";
+
+// frase  = 'mi nombre es "carlos" y soy perfect';
+
+// document.write(frase);
+
+
+
