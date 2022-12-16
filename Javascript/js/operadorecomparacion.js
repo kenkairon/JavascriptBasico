ff69acf5faeb767c510222d3cda1312a9addf284
kenkairon <ingeinfo2006@gmail.com>
2022-12-16 15:29:46 -0300

    Operadores de comparacion x carlos vasquez aun falta información

---
+++ Javascript/js/operadorecomparacion.js
@@ -0,0 +1,20 @@
+//Operadores de Comparación 
+//Los operadores de comparación comparan dos expresiones y devuelve un valor
+// boolean que representa la relación de sus valores true o false
+
+// let numero = 23;
+// let numero2 = 13;
+// document.write(numero == numero2);
+
+// Los operadores sean distinto por texto
+// let texto = "texto1";
+// let texto2 = "texto2";
+// document.write(texto != texto2);
+
+// === verifica si los numeros son exactamente iguales en valor y declaración
+let numero = 23;
+let texto = "23";
+// == con dos iguales comparas si son iguales
+document.write(numero == texto);
+// == con tres iguales comparas si son extrictamente iguale
+document.write(numero === texto);
